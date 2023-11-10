f280d7de4b9b0c2dc171a11e9d9bd632d179dc79
helgacozzo <helgacozzo@gmail.com>
2023-11-10 11:53:30 +0100

    fix/ primo esercizio

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
 window.addEventListener('load', function() {
 
     function analizzaTesto(stringa) {
-
-      const spazi = stringa.trim();
-      const minuscolo = spazi.toLowerCase();
-      const contienePezziStringa = spazi.includes("IS") || spazi.includes("JavaScript");
-      const numeroCaratteri = spazi.length;
+      
+      const stringaOriginale = stringa;
+      const minuscolo = stringaOriginale.toLowerCase();
+      const spazi = stringaOriginale.trim();
+      const contienePezziStringa = stringaOriginale.includes("IS") || stringaOriginale.includes("JavaScript");
+      const numeroCaratteri = stringaOriginale.length;
 
       let invertiStringa = '';
-      for (let i = spazi.length - 1; i >= 0; i--) {
-        invertiStringa += spazi[i];
+      for (let i = stringaOriginale.length - 1; i >= 0; i--) {
+        invertiStringa += stringaOriginale[i];
       }
+
+      const risultato = {
+        originale: stringaOriginale,
+        minuscolo: minuscolo,
+        senzaSpazi: spazi,
+        numeroCaratteri: numeroCaratteri,
+        contieneJS: contienePezziStringa,
+        invertita: invertiStringa,
+      };
+      
+      return risultato;
+    }
+  
+    const risultato = analizzaTesto('Ecco il risultato!');
+    console.log(risultato);
     
   });
-  
